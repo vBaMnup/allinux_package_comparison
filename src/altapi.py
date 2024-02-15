adedbd9cce540a0737610bb24de744144ff71a6e
vbamnup <vbamnup@gmail.com>
2024-02-15 12:38:05 +0300

    Add altapi class

--- src/altapi.py
+++ src/altapi.py
@@ -2,12 +2,20 @@ import aiohttp
 
 
 class AltApi:
+    """AltApi class"""
+
     def __init__(self, session: aiohttp.ClientSession):
+        """
+        Initialize AltApi class
+        """
         self.base_url = "https://rdb.altlinux.org/api/"
 
-        async def get_branch_binary_packages(branch):
-            url = f"{self.base_url}/export/branch_binary_packages/{branch}"
+    async def get_branch_binary_packages(branch):
+        """
+        Get branch binary packages
+        """
+        url = f"{self.base_url}/export/branch_binary_packages/{branch}"
 
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url) as response:
-                    return await response.json()
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                return await response.json()
