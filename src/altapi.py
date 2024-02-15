23731abbd4052b3b725c57af03acc52b84075e0e
vbamnup <vbamnup@gmail.com>
2024-02-15 13:05:15 +0300

    add compare packages;

--- src/altapi.py
+++ src/altapi.py
@@ -1,7 +1,7 @@
 import aiohttp
 
 
-class AltApi:
+class AltLinuxAPI:
     """AltApi class"""
 
     def __init__(self, session: aiohttp.ClientSession):
@@ -10,7 +10,7 @@ class AltApi:
         """
         self.base_url = "https://rdb.altlinux.org/api/"
 
-    async def get_branch_binary_packages(branch):
+    async def get_branch_binary_packages(self, branch):
         """
         Get branch binary packages
         """
